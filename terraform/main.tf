11e2b31eac5029f0dcaa0626437515c57f198dff
Hugo <hugotinoco@icloud.com>
2022-05-28 14:01:01 +0000

    tform deploying

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,28 +1,10 @@
-#Policy document specifying what service can assume the role
-data "aws_iam_policy_document" "assume_role" {
-  statement {
-    effect  = "Allow"
-    actions = ["sts:AssumeRole"]
-    principals {
-      type        = "Service"
-      identifiers = ["amplify.amazonaws.com"]
-    }
-  }
-}
-
-#IAM role providing read-only access to CodeCommit
-resource "aws_iam_role" "amplify-codecommit" {
-  name                = "AmplifyCodeCommit"
-  assume_role_policy  = join("", data.aws_iam_policy_document.assume_role.*.json)
-  managed_policy_arns = ["arn:aws:iam::aws:policy/AWSCodeCommitReadOnly"]
-}
-
-resource "aws_amplify_app" "the-best-app" {
-  name       = "Dev-Knot Blog"
-  repository = "https://github.com/h4ndzdatm0ld/dev-knot-blog"
-  iam_service_role_arn = aws_iam_role.amplify-codecommit.arn
+resource "aws_amplify_app" "dev-knot-app" {
+  name                     = var.blog_name
+  repository               = var.repository
+  access_token             = var.gh_access_token
   enable_branch_auto_build = true
-  build_spec = <<-EOT
+  platform                 = "WEB"
+  build_spec               = <<-EOT
     version: 0.1
     frontend:
       phases:
@@ -47,3 +29,63 @@ resource "aws_amplify_app" "the-best-app" {
     ENV = "dev"
   }
 }
+# ADD Branch setup to new AWS Amplify APP Resource
+resource "aws_amplify_branch" "main" {
+  app_id      = aws_amplify_app.dev-knot-app.id
+  branch_name = "main"
+
+  # framework = "React"
+  stage     = "PRODUCTION"
+}
+resource "aws_amplify_branch" "develop" {
+  app_id      = aws_amplify_app.dev-knot-app.id
+  branch_name = "develop"
+
+  # framework = "React"
+  stage     = "DEVELOPMENT"
+}
+# ADD Webhooks
+resource "aws_amplify_webhook" "main" {
+  app_id      = aws_amplify_app.dev-knot-app.id
+  branch_name = aws_amplify_branch.main.branch_name
+  description = "amplify-hook-main"
+}
+resource "aws_amplify_webhook" "DEVELOPMENT" {
+  app_id      = aws_amplify_app.dev-knot-app.id
+  branch_name = aws_amplify_branch.develop.branch_name
+  description = "amplify-hook-develop"
+}
+# # ACM Certificate
+# resource "aws_acm_certificate" "blog" {
+#   provider          = aws.us-west-2
+#   domain_name       = var.blog_domain
+#   validation_method = "DNS"
+
+#   lifecycle {
+#     create_before_destroy = true
+#   }
+# }
+
+
+# resource "aws_route53_record" "blog_cert" {
+#   for_each = {
+#   for dvo in aws_acm_certificate.blog.domain_validation_options : dvo.domain_name => {
+#     name   = dvo.resource_record_name
+#     record = dvo.resource_record_value
+#     type   = dvo.resource_record_type
+#   }
+#   }
+
+#   allow_overwrite = true
+#   name            = each.value.name
+#   records         = [each.value.record]
+#   ttl             = 300
+#   type            = each.value.type
+#   zone_id         = data.terraform_remote_state.bootstrap.outputs.route53_hosted_zone_id
+# }
+
+# resource "aws_acm_certificate_validation" "blog_cert" {
+#   provider          = aws.us-west-2
+#   certificate_arn         = aws_acm_certificate.blog.arn
+#   validation_record_fqdns = [for record in aws_route53_record.blog_cert : record.fqdn]
+# }
